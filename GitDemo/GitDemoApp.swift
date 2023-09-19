f766ed04d600476e8ce000dbbb1ec2ccdc2c6116
Sneha Kalariya <sneha.kalaria@gmail.com>
2023-09-19 08:02:31 -0700

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Sneha Kalariya on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
